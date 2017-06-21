44b4453feb2c37b266af3c3215eab412f1229b7a
AnonUser676 <rouie.ortega@live.com>
2017-06-21 16:24:49 +1000

    Modified and added Object Pool for Snowflakes and Letters, finished Splash State to implement State Machine and create other States.

--- Project2/aieBootstrap/project2D/Application2D.h
+++ Project2/aieBootstrap/project2D/Application2D.h
@@ -19,10 +19,6 @@ public:
 protected:
 
 	aie::Renderer2D*	m_2dRenderer;
-	aie::Texture*		m_shipTexture;
-	aie::Font*			m_font;
-	aie::Audio*			m_audio;
 
-	float m_cameraX, m_cameraY;
 	float m_timer;
 };
--- Project2/aieBootstrap/project2D/main.cpp
+++ Project2/aieBootstrap/project2D/main.cpp
@@ -1,7 +1,12 @@
 #include "Application2D.h"
+#include <time.h>
+#include <windows.h>
+#include <crtdbg.h>
 
 int main() 
 {
+	srand((unsigned int)time(0));
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Application2D* app = new Application2D();
 	app->run("AIE", 1280, 720, false);
 	delete app;
